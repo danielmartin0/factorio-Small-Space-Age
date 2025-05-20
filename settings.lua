f84ce586eeec920d017842da558dcac2539c0227
danielmartin0 <github@danielgmartin.com>
2025-05-20 12:28:01 +0100

    see changelog.txt

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend({
 		type = "double-setting",
 		name = "smallspage-radius",
 		setting_type = "startup",
-		default_value = 330,
+		default_value = 315,
 		minimum_value = 20,
 		maximum_value = 100000,
 	},
